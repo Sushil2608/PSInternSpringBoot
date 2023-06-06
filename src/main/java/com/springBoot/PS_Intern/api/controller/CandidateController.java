5f4bdf8fb9e25ec8cfb02a563b9f837d7474ca89
Shobhit Agarwal <shoagarw2@publicisgroupe.net>
2023-06-06 19:00:56 +0530

    Added logger example in controller

--- src/main/java/com/springBoot/PS_Intern/api/controller/CandidateController.java
+++ src/main/java/com/springBoot/PS_Intern/api/controller/CandidateController.java
@@ -1,7 +1,7 @@
 package com.springBoot.PS_Intern.api.controller;
 
-import java.util.Optional;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +17,15 @@ import org.springframework.http.ResponseEntity;
 import com.springBoot.PS_Intern.api.model.Candidate;
 import com.springBoot.PS_Intern.service.CandidateService;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @RestController
+@Slf4j
 @RequestMapping("/api")
 public class CandidateController {
+	
+	private static Logger log = LoggerFactory.getLogger(CandidateController.class);
 	
 	private CandidateService candidateService;
 	
@@ -33,7 +37,7 @@ public class CandidateController {
 	//POST REQUEST
 	@PostMapping(value = "/candidate")
 	public ResponseEntity<Candidate> saveCandidate(@RequestBody Candidate candidate) {
-		
+		log.info("Request received to create candidate with name {} ", candidate.getFirstName());
 		return new ResponseEntity<Candidate>(candidateService.saveCandidate(candidate), HttpStatus.CREATED);
 	}
 	
